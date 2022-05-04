4b07f29fbd4489723c406effc788488718ef9917
konain1 <barcafchd@gmail.com>
2022-05-04 13:53:06 +0530

    nto1backtracking

---
+++ recursion/printNto1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class printNto1 {
+    
+public static void ntimes(int i,int n){
+    if(i<1){
+        return;
+    }
+    System.out.println(i);
+    ntimes(i-1,n);
+    
+}
+
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        ntimes(n,n);
+        
+    }
+}
